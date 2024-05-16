114b32dfc22cffb44f9b291557afe0c6f28f7a05
Mukesh-2005 <starmukesh2005@gmail.com>
2024-05-16 21:59:26 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data science tools and Ecosystem
+# 
+
+# In this notebook, Data Science tools and Ecosystem are summarized
+
+# __Objectives:__
+# 
+# list the popular languages for Data science:
+# - Python 
+# - R
+# - Java
+# - julia 
+# - scala
+
+# Sum of the most popular languages in the Data Scienctist Include:
+# 1. Python 
+# 2. R
+# 3. Java 
+
+# Some of the most commonly used lybraries in Data Science include:
+# 
+# 1. Numpy 
+# 2. pandas 
+# 3. Matplotlib
+# 
+
+# |Data Science Tools| 
+# |------------------|
+# | Hadoop |
+# | Apache |
+# | spark |
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[4]:
+
+
+#This a simple arithmetic expression to mutiply then add integers.
+(4*3)+5
+
+
+# In[5]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+(200/60)
+
+
+# ## Author
+# K. Mukesh
+
+# In[ ]:
+
+
+
+
